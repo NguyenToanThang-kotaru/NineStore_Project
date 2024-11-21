e8a159c57edcfb4e5033a03808b49bc50266ef56
unknown <NguyenToanThang15122005@gmail.com>
2024-11-21 11:31:54 +0700

    update

---
+++ Downloads/NineStore_Project-update/NineStore_Project-new2/NineStore_Project-new2/common.js
@@ -0,0 +1,16 @@
+// -------------------------------- hidden Orverlay ------------------------------
+const overlayArr = document.querySelectorAll(".overlay");
+overlayArr.forEach((overlays) => {
+  overlays.addEventListener("click", (event) => {
+    if (event.target === overlays) overlays.style.display = "none";
+  });
+});
+const closes = document.querySelectorAll(".close");
+closes.forEach((closebtn) => {
+  closebtn.addEventListener("click", () => {
+    closebtn.parentElement.parentElement.style.display = "none";
+  });
+});
+function reloadPage() {
+  window.location.href = "Nineshop.html";
+}
